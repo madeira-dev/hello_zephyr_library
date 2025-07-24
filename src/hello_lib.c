75bdccb30043ee063e44f96278dda6185a902418
Gabriel Madeira <gabrielmadeira2002@gmail.com>
2025-07-24 11:38:34 -0300

    this setup is successfully building and being imported into the testing app for this library

--- src/hello_lib.c
+++ src/hello_lib.c
@@ -6,8 +6,7 @@ LOG_MODULE_REGISTER(hello_lib, LOG_LEVEL_DBG);
 
 #include "hello_lib.h"
 
-void hello_lib_say_hello(void)
-{
-    // Using Zephyr's logging API is better than printk
-    LOG_INF("Hello World from my awesome Zephyr library!");
+void hello_lib_say_hello(void) {
+  // Using Zephyr's logging API is better than printk
+  LOG_INF("Hello World from my awesome Zephyr library!");
 }
