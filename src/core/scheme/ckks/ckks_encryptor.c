3ce0a80f1b01dbc2bba3fa1be222507a81c49b2f
Gabriel Madeira <gabrielmadeira2002@gmail.com>
2025-07-28 17:34:15 -0300

    More debug

--- src/core/scheme/ckks/ckks_encryptor.c
+++ src/core/scheme/ckks/ckks_encryptor.c
@@ -28,9 +28,10 @@ int ckks_encrypt(const ckks_cryptoparams_t *params,
 
     // 1. Sample random u (binary polynomial)
     polynomial_t u;
+    LOG_INF("poly_mult(&u, N - 1) started");
     if (poly_init(&u, N - 1) != 0)
     {
-        LOG_ERR("poly_init(u) failed");
+        LOG_ERR("poly_init(&u, N - 1) failed");
         return -1;
     }
     for (uint32_t i = 0; i < N; i++)
@@ -81,27 +82,33 @@ int ckks_encrypt(const ckks_cryptoparams_t *params,
 
     // 3. Compute c[0] = pk[0]*u + e0 + m
     polynomial_t pk0u, c0;
+    LOG_INF("poly_init(pk0u) started");
     if (poly_init(&pk0u, N - 1) != 0)
     {
         LOG_ERR("poly_init(pk0u) failed");
         return -1;
     }
+
+    LOG_INF("poly_mult(pk0u, pk[0], u) started");
     if (poly_mult(&pk0u, &pk->pk[0], &u, ring) != 0)
     {
         LOG_ERR("poly_mult(pk0u, pk[0], u) failed");
         return -1;
     }
 
+    LOG_INF("poly_init(c0) started");
     if (poly_init(&c0, N - 1) != 0)
     {
         LOG_ERR("poly_init(c0) failed");
         return -1;
     }
+    LOG_INF("poly_add(c0, pk0u, e0) started");
     if (poly_add(&c0, &pk0u, &e0, ring) != 0)
     {
         LOG_ERR("poly_add(c0, pk0u, e0) failed");
         return -1;
     }
+    LOG_INF("poly_add(c0, c0, plaintext) started");
     if (poly_add(&c0, &c0, &plaintext->poly, ring))
     {
         LOG_ERR("poly_add(c0, c0, plaintext) failed");
@@ -110,22 +117,26 @@ int ckks_encrypt(const ckks_cryptoparams_t *params,
 
     // 4. Compute c[1] = pk[1]*u + e1
     polynomial_t pk1u, c1;
+    LOG_INF("poly_init(pk1u) started");
     if (poly_init(&pk1u, N - 1) != 0)
     {
         LOG_ERR("poly_init(pk1u) failed");
         return -1;
     }
+    LOG_INF("poly_mult(pk1u, pk[1], u) started");
     if (poly_mult(&pk1u, &pk->pk[1], &u, ring))
     {
         LOG_ERR("poly_mult(pk1u, pk[1], u) failed");
         return -1;
     }
 
+    LOG_INF("poly_init(c1) started");
     if (poly_init(&c1, N - 1) != 0)
     {
         LOG_ERR("poly_init(c1) failed");
         return -1;
     }
+    LOG_INF("poly_add(c1, pk1u, e1) started");
     if (poly_add(&c1, &pk1u, &e1, ring))
     {
         LOG_ERR("poly_add(c1, pk1u, e1) failed");
@@ -133,11 +144,13 @@ int ckks_encrypt(const ckks_cryptoparams_t *params,
     }
 
     // 5. Output ciphertext
+    LOG_INF("poly_copy(ciphertext->parts[0], c0) started");
     if (poly_copy(&ciphertext->parts[0], &c0))
     {
         LOG_ERR("poly_copy(ciphertext->parts[0], c0) failed");
         return -1;
     }
+    LOG_INF("poly_copy(ciphertext->parts[1], c1) started");
     if (poly_copy(&ciphertext->parts[1], &c1))
     {
         LOG_ERR("poly_copy(ciphertext->parts[1], c1) failed");
